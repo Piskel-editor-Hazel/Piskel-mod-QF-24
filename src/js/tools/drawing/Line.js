95a92c4f3ebd56a63b9bf6c5018df02335b26185
Piskel-editor-Hazel <smokehorn587@gmail.com>
2024-10-14 10:59:16 -0500

    Update and rename Stroke.js to Line.js and change line formula to make second section of the line longer
    Hopefully this works, will change if it doesnt

--- src/js/tools/drawing/Line.js
+++ src/js/tools/drawing/Line.js
@@ -1,62 +1,62 @@
 /**
- * @provide pskl.tools.drawing.Stroke
+ * @provide pskl.tools.drawing.Line
  *
  * @require pskl.utils
  */
 (function() {
   var ns = $.namespace('pskl.tools.drawing');
 
-  ns.Stroke = function() {
-    this.toolId = 'tool-stroke';
-    this.helpText = 'Stroke tool';
+  ns.Line = function() {
+    this.toolId = 'tool-line';
+    this.helpText = 'Line tool';
     this.shortcut = pskl.service.keyboard.Shortcuts.TOOL.STROKE;
     this.tooltipDescriptors = [
       {key : 'shift', description : 'Hold shift to draw straight lines'}
     ];
 
-    // Stroke's first point coordinates (set in applyToolAt)
+    // Line's first point coordinates (set in applyToolAt)
     this.startCol = null;
     this.startRow = null;
   };
 
-  pskl.utils.inherit(ns.Stroke, ns.BaseTool);
+  pskl.utils.inherit(ns.Line, ns.BaseTool);
 
-  ns.Stroke.prototype.supportsDynamicPenSize = function() {
+  ns.Line.prototype.supportsDynamicPenSize = function() {
     return true;
   };
 
   /**
    * @override
    */
-  ns.Stroke.prototype.applyToolAt = function(col, row, frame, overlay, event) {
+  ns.Line.prototype.applyToolAt = function(col, row, frame, overlay, event) {
     this.startCol = col;
     this.startRow = row;
 
-    // When drawing a stroke we don't change the model instantly, since the
-    // user can move his cursor to change the stroke direction and length
-    // dynamically. Instead we draw the (preview) stroke in a fake canvas that
+    // When drawing a line we don't change the model instantly, since the
+    // user can move his cursor to change the line direction and length
+    // dynamically. Instead we draw the (preview) line in a fake canvas that
     // overlay the drawing canvas.
     // We wait for the releaseToolAt callback to impact both the
     // frame model and canvas rendering.
 
-    // The fake canvas where we will draw the preview of the stroke:
-    // Drawing the first point of the stroke in the fake overlay canvas:
+    // The fake canvas where we will draw the preview of the line:
+    // Drawing the first point of the line in the fake overlay canvas:
     overlay.setPixel(col, row, this.getToolColor());
   };
 
-  ns.Stroke.prototype.moveToolAt = function(col, row, frame, overlay, event) {
+  ns.Line.prototype.moveToolAt = function(col, row, frame, overlay, event) {
     overlay.clear();
 
     var penSize = pskl.app.penSizeService.getPenSize();
     var isStraight = event.shiftKey;
     var color = this.getToolColor();
     if (color == Constants.TRANSPARENT_COLOR) {
-      // When mousemoving the stroke tool, we draw in the canvas overlay above the drawing canvas.
-      // If the stroke color is transparent, we won't be
+      // When mousemoving the line tool, we draw in the canvas overlay above the drawing canvas.
+      // If the line color is transparent, we won't be
       // able to see it during the movement.
-      // We set it to a semi-opaque white during the tool mousemove allowing to see colors below the stroke.
-      // When the stroke tool will be released, It will draw a transparent stroke,
-      // eg deleting the equivalent of a stroke.
+      // We set it to a semi-opaque white during the tool mousemove allowing to see colors below the line.
+      // When the line tool will be released, It will draw a transparent line,
+      // eg deleting the equivalent of a line.
       color = Constants.SELECTION_TRANSPARENT_COLOR;
     }
 
@@ -66,7 +66,7 @@
   /**
    * @override
    */
-  ns.Stroke.prototype.releaseToolAt = function(col, row, frame, overlay, event) {
+  ns.Line.prototype.releaseToolAt = function(col, row, frame, overlay, event) {
     var penSize = pskl.app.penSizeService.getPenSize();
     var isStraight = event.shiftKey;
     var color = this.getToolColor();
@@ -75,7 +75,7 @@
     // the model and draw them in the drawing canvas (not the fake overlay anymore)
     this.draw_(col, row, color, frame, penSize, isStraight);
 
-    // For now, we are done with the stroke tool and don't need an overlay anymore:
+    // For now, we are done with the line tool and don't need an overlay anymore:
     overlay.clear();
 
     this.raiseSaveStateEvent({
@@ -89,7 +89,7 @@
     });
   };
 
-  ns.Stroke.prototype.draw_ = function (col, row, color, targetFrame, penSize, isStraight) {
+  ns.Line.prototype.draw_ = function (col, row, color, targetFrame, penSize, isStraight) {
     var linePixels;
     if (isStraight) {
       linePixels = pskl.PixelUtils.getUniformLinePixels(this.startCol, col, this.startRow, row);
@@ -100,7 +100,7 @@
     //draw the square ends of the line
     pskl.PixelUtils.resizePixel(linePixels[0].col, linePixels[0].row, penSize)
       .forEach(function (point) {targetFrame.setPixel(point[0], point[1], color);});
-    pskl.PixelUtils.resizePixel(linePixels[linePixels.length - 1].col, linePixels[linePixels.length - 1].row, penSize)
+    pskl.PixelUtils.resizePixel(linePixels[linePixels.length].col, linePixels[linePixels.length].row, penSize)
       .forEach(function (point) {targetFrame.setPixel(point[0], point[1],color);});
 
     //for each step along the line, draw an x centered on that pixel of size penSize
@@ -125,7 +125,7 @@
     });
   };
 
-  ns.Stroke.prototype.replay = function(frame, replayData) {
+  ns.Line.prototype.replay = function(frame, replayData) {
     this.startCol = replayData.startCol;
     this.startRow = replayData.startRow;
     this.draw_(replayData.col, replayData.row, replayData.color, frame, replayData.penSize, replayData.isStraight);
